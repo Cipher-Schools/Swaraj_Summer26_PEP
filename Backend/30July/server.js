6e7cd5ba6b8bdcd78963c6e9319579f0313d1a69
SwarajAnand <swrjnnd@gmail.com>
2026-07-30 15:51:59 +0530

    Added basic folder for Rag Bot & Theory

---
+++ Backend/30July/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+const fs = require("fs");
+const app = express();
+
+app.post("/upload", upload.single("pdf"), async (req, res) => {
+//   console.log(req.file);
+
+  if (!req.file) {
+    res.send("Please upload any PFD file");
+  }
+
+  if (req.file.mimetype !== "application/pdf") {
+    res.send("Please upload only PDF file");
+  }
+
+  try {
+    const buffer = fs.readFileSync(req.file.path);
+    console.log(buffer);
+
+    // Rag fn to upload file
+
+    fs.unlinkSync(req.file.path);
+  } catch (err) {
+    res.send(`Unexpected Error Occured ${err}`);
+  }
+
+  res.send("File uploaded successfully");
+});
+
+app.listen(3000, () => console.log("Server is running on port 3000"));
